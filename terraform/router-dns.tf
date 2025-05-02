53f35aab5cacc69a4b8b1f7c739ae495e37dd659
Giorgio Aresu <giorgioaresu@gmail.com>
2025-05-02 23:44:19 +0200

    Add static mappings for IoT devices

--- terraform/router-dns.tf
+++ terraform/router-dns.tf
@@ -5,3 +5,59 @@ resource "routeros_ip_dns_record" "m5stack_atom_echo" {
   name = "m5stack-atom-echo.iot.aresu.eu"
   address = routeros_ip_dhcp_server_lease.m5stack_atom_echo.address
 }
+resource "routeros_ip_dns_record" "bagno_principale" {
+  provider    = routeros.rb5009
+  comment     = "Managed by Terraform"
+  type = "A"
+  name = "bagno-principale.aresu.eu"
+  address = routeros_ip_dhcp_server_lease.bagno_principale.address
+}
+resource "routeros_ip_dns_record" "dehumidifier" {
+  provider    = routeros.rb5009
+  comment     = "Managed by Terraform"
+  type = "A"
+  name = "dehumidifier.iot.aresu.eu"
+  address = routeros_ip_dhcp_server_lease.dehumidifier.address
+}
+resource "routeros_ip_dns_record" "teckin" {
+  provider    = routeros.rb5009
+  comment     = "Managed by Terraform"
+  type = "A"
+  name = "teckin.iot.aresu.eu"
+  address = routeros_ip_dhcp_server_lease.teckin.address
+}
+resource "routeros_ip_dns_record" "tplink_hs110" {
+  provider    = routeros.rb5009
+  comment     = "Managed by Terraform"
+  type = "A"
+  name = "hs110.iot.aresu.eu"
+  address = routeros_ip_dhcp_server_lease.tplink_hs110.address
+}
+resource "routeros_ip_dns_record" "sonoff_rf_bridge" {
+  provider    = routeros.rb5009
+  comment     = "Managed by Terraform"
+  type = "A"
+  name = "rf-bridge.iot.aresu.eu"
+  address = routeros_ip_dhcp_server_lease.sonoff_rf_bridge.address
+}
+resource "routeros_ip_dns_record" "stufetta" {
+  provider    = routeros.rb5009
+  comment     = "Managed by Terraform"
+  type = "A"
+  name = "stufetta.iot.aresu.eu"
+  address = routeros_ip_dhcp_server_lease.stufetta.address
+}
+resource "routeros_ip_dns_record" "ble_tracker" {
+  provider    = routeros.rb5009
+  comment     = "Managed by Terraform"
+  type = "A"
+  name = "ble-tracker.iot.aresu.eu"
+  address = routeros_ip_dhcp_server_lease.ble_tracker.address
+}
+resource "routeros_ip_dns_record" "persiana" {
+  provider    = routeros.rb5009
+  comment     = "Managed by Terraform"
+  type = "A"
+  name = "persiana.iot.aresu.eu"
+  address = routeros_ip_dhcp_server_lease.persiana.address
+}
