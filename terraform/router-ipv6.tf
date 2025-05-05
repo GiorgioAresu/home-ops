a0d02f564f740db40b84b25c88a12c3b15fd0c65
Giorgio Aresu <giorgioaresu@gmail.com>
2025-05-05 16:33:06 +0200

    feat(network): IPv6 config

---
+++ terraform/router-ipv6.tf
@@ -0,0 +1,14 @@
+resource "routeros_ipv6_settings" "settings" {
+  provider     = routeros.rb5009
+  disable_ipv6 = false
+}
+
+# https://www.reddit.com/r/mikrotik/comments/16410a1/comment/jy8tl37/
+
+resource "routeros_ipv6_neighbor_discovery" "lan" {
+  provider  = routeros.rb5009
+  comment   = "Managed by Terraform"
+  interface = "iot"
+  dns       = "fd26::1"
+}
+
