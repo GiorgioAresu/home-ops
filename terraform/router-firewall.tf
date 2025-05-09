cb153492a959f3c41440fa1f1cabdf61a946d3a1
Giorgio Aresu <giorgioaresu@gmail.com>
2025-05-09 08:08:12 +0200

    feat(network): add NAT rules

--- terraform/router-firewall.tf
+++ terraform/router-firewall.tf
@@ -1,6 +1,66 @@
+### NAT
+resource "routeros_ip_firewall_nat" "transmission_tcp" {
+  provider          = routeros.rb5009
+  comment           = "Managed by Terraform - Transmission TCP"
+  disabled          = false
+  action            = "dst-nat"
+  chain             = "dstnat"
+  protocol          = "tcp"
+  in_interface_list = routeros_interface_list.wan.name
+  dst_port          = 51413
+  to_addresses      = "10.17.1.35"
+}
+resource "routeros_ip_firewall_nat" "transmission_udp" {
+  provider          = routeros.rb5009
+  comment           = "Managed by Terraform - Transmission UDP"
+  disabled          = false
+  action            = "dst-nat"
+  chain             = "dstnat"
+  protocol          = "udp"
+  in_interface_list = routeros_interface_list.wan.name
+  dst_port          = 51413
+  to_addresses      = "10.17.1.35"
+}
+
+resource "routeros_ip_firewall_nat" "syncthing_tcp" {
+  provider          = routeros.rb5009
+  comment           = "Managed by Terraform - SyncThing TCP"
+  disabled          = false
+  action            = "dst-nat"
+  chain             = "dstnat"
+  protocol          = "tcp"
+  in_interface_list = routeros_interface_list.wan.name
+  dst_port          = 22000
+  to_addresses      = "10.17.1.35"
+}
+resource "routeros_ip_firewall_nat" "syncthing_udp" {
+  provider          = routeros.rb5009
+  comment           = "Managed by Terraform - SyncThing UDP"
+  disabled          = false
+  action            = "dst-nat"
+  chain             = "dstnat"
+  protocol          = "udp"
+  in_interface_list = routeros_interface_list.wan.name
+  dst_port          = 22000
+  to_addresses      = "10.17.1.35"
+}
+
+resource "routeros_ip_firewall_nat" "ingress" {
+  provider          = routeros.rb5009
+  comment           = "Managed by Terraform - Ingress"
+  disabled          = false
+  action            = "dst-nat"
+  chain             = "dstnat"
+  protocol          = "tcp"
+  in_interface_list = routeros_interface_list.wan.name
+  dst_port          = 443
+  to_addresses      = "10.17.1.201"
+}
+
 resource "routeros_ip_firewall_nat" "qbittorrent_tcp" {
   provider          = routeros.rb5009
   comment           = "Managed by Terraform - QBittorrent TCP"
+  disabled          = false
   action            = "dst-nat"
   chain             = "dstnat"
   protocol          = "tcp"
@@ -11,10 +71,36 @@ resource "routeros_ip_firewall_nat" "qbittorrent_tcp" {
 resource "routeros_ip_firewall_nat" "qbittorrent_udp" {
   provider          = routeros.rb5009
   comment           = "Managed by Terraform - QBittorrent UDP"
+  disabled          = false
   action            = "dst-nat"
   chain             = "dstnat"
   protocol          = "udp"
   in_interface_list = routeros_interface_list.wan.name
   dst_port          = 52015
   to_addresses      = "10.17.1.216"
+}
+
+resource "routeros_ip_firewall_nat" "ntp" {
+  provider          = routeros.rb5009
+  comment           = "Managed by Terraform - Redirect all NTP to local"
+  disabled          = false
+  action            = "dst-nat"
+  chain             = "dstnat"
+  protocol          = "udp"
+  in_interface_list = routeros_interface_list.wan.name
+  dst_port          = 123
+  to_addresses      = "10.17.1.1"
+  log = false
+  log_prefix = ""
+}
+resource "routeros_ip_firewall_nat" "dns" {
+  provider          = routeros.rb5009
+  comment           = "Managed by Terraform - Redirect all DNS to local"
+  disabled          = true
+  action            = "dst-nat"
+  chain             = "dstnat"
+  protocol          = "udp"
+  in_interface_list = routeros_interface_list.wan.name
+  dst_port          = 53
+  to_addresses      = "10.17.1.1"
 }
