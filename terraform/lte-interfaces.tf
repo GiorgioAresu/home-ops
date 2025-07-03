cd48be2371fda4e2b4c6320f856de63e52fa3139
Giorgio Aresu <giorgioaresu@gmail.com>
2025-07-04 00:04:27 +0200

    feat(network): add lte interfaces to lte device

--- terraform/lte-interfaces.tf
+++ terraform/lte-interfaces.tf
@@ -1,3 +1,15 @@
+# =================================================================================================
+# Bridge Interfaces
+# https://registry.terraform.io/providers/terraform-routeros/routeros/latest/docs/resources/interface_bridge
+# =================================================================================================
+resource "routeros_interface_bridge" "lte_bridge" {
+  provider = routeros.hAP_ax_lite_LTE6
+  comment  = "defconf - Managed by Terraform"
+  name     = "bridgeLocal"
+  # vlan_filtering = true
+}
+
+
 resource "routeros_interface_lte_apn" "vodafone" {
   provider      = routeros.hAP_ax_lite_LTE6
   comment       = "Managed by Terraform"
---
+++ terraform/lte-vlans.tf
@@ -0,0 +1,13 @@
+resource "routeros_interface_vlan" "lte_lte" {
+  provider  = routeros.hAP_ax_lite_LTE6
+  comment   = "Managed by Terraform"
+  interface = routeros_interface_bridge.lte_bridge.name
+  name      = "lte"
+  vlan_id   = 2
+}
+resource "routeros_interface_bridge_vlan" "lte_lte" {
+  provider = routeros.hAP_ax_lite_LTE6
+  comment  = "Managed by Terraform"
+  bridge   = routeros_interface_bridge.lte_bridge.name
+  vlan_ids = [routeros_interface_vlan.lte_lte.vlan_id]
+}
