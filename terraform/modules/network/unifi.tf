7441ea66cbb0875313bd7a6dfe691cc04784715b
Giorgio Aresu <giorgioaresu@gmail.com>
2025-07-16 23:22:46 +0200

    chore(network): use switch port 5 for IoT

--- terraform/modules/network/unifi.tf
+++ terraform/modules/network/unifi.tf
@@ -19,14 +19,21 @@ resource "unifi_port_profile" "guest" {
   poe_mode              = "off"
 }
 
+resource "unifi_port_profile" "iot" {
+  name                  = "IoT"
+  native_networkconf_id = unifi_network.iot.id
+  forward               = "native"
+  poe_mode              = "off"
+}
+
 resource "unifi_device" "usw_flex_mini" {
   mac  = "74:83:c2:0f:33:4c"
   name = "USW Flex Mini (Desk)"
 
   port_override {
     number          = 5
-    name            = "Guest"
-    port_profile_id = unifi_port_profile.guest.id
+    name            = "IoT"
+    port_profile_id = unifi_port_profile.iot.id
   }
 }
 
