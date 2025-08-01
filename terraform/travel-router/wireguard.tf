445aa5be80110148d516facc51b5712da9ade06b
Giorgio Aresu <giorgioaresu@gmail.com>
2025-08-01 08:17:59 +0200

    fix: wireguard travel connection

--- terraform/travel-router/wireguard.tf
+++ terraform/travel-router/wireguard.tf
@@ -20,10 +20,36 @@ resource "routeros_interface_wireguard_peer" "home" {
   comment              = "Managed by Terraform - Home"
   allowed_address      = ["0.0.0.0/0"]
   endpoint_address     = var.wireguard_home_endpoint
+  endpoint_port        = 13231
   disabled             = false
   interface            = routeros_interface_wireguard.travel.name
   name                 = "Home"
   persistent_keepalive = null
   preshared_key        = var.wireguard_home_preshared_key
   public_key           = var.wireguard_home_public_key
+  is_responder         = "true"
 }
+
+# ================================================================================================
+# IP Address
+# https://registry.terraform.io/providers/terraform-routeros/routeros/latest/docs/resources/ip_address
+# ================================================================================================
+resource "routeros_ip_address" "home" {
+  comment   = "Managed by Terraform"
+  address   = "10.17.100.5/24"
+  interface = routeros_interface_wireguard.travel.name
+}
+
+
+resource "routeros_ip_firewall_nat" "wireguard" {
+  comment       = "Managed by Terraform - wireguard masquerade"
+  chain         = "srcnat"
+  action        = "masquerade"
+  out_interface = routeros_interface_wireguard.travel.name
+}
+
+resource "routeros_ip_route" "home" {
+  dst_address = "10.17.1.0/24"
+  gateway     = routeros_interface_wireguard.travel.name
+}
+
