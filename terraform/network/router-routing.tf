9637037f532086cc749496a630ce7452f9f16a44
GiorgioAresu <giorgioaresu@gmail.com>
2026-08-30 22:14:59 +0200

    feat: rework and upgrades post-nuke

--- terraform/network/router-routing.tf
+++ terraform/network/router-routing.tf
@@ -50,10 +50,10 @@ resource "routeros_routing_bgp_connection" "home_ops_kube_proxmox_01" {
   }
 }
 
-resource "routeros_routing_bgp_connection" "home_ops_kube_minisforum_01" {
+resource "routeros_routing_bgp_connection" "home_ops_delta" {
   provider       = routeros.rb5009
-  comment        = "Managed by Terraform - kube-minisforum-01"
-  name           = "cilium-bgp-minisforum-01"
+  comment        = "Managed by Terraform - [delta] MinisForum UH125 Pro"
+  name           = "cilium-bgp-delta"
   as             = local.bgp_as_local
   listen         = true
   nexthop_choice = "force-self"
